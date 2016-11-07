58052461de02b6e4133f97ddd9bae2965d805f15
yfujimot <yosh.fuj@gmail.com>
2016-11-07 15:36:59 -0500

    init

---
+++ app/src/main/java/com/thinkfuji/cmsc434_doodle/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thinkfuji.cmsc434_doodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
